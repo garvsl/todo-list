6d0123492bd828c45b449572bc22d759e583ccea
garvsl <garvsehgal7@gmail.com>
2023-01-01 02:42:33 -0500

    basic javascript

--- main.js
+++ main.js
@@ -7,26 +7,43 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log('hello')\n\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n\n\nfunction createProject(name, todo) {\n    return {\n        name: 'Default',\n        todo,\n    }\n}\n\nfunction createTodo(title, description, dueDate, priority) {\n    return {\n        title,\n        description,\n        dueDate,\n        priority,\n        status: false,\n    }\n}\n\nfunction todoStatus(todo) {\n    todo.todo.status = true\n    return todo.todo\n}\n\nconst gym = createTodo('gym', 'go to the gym', '1/1/23', 'now')\nconst normal = createProject('Default', gym)\n\nconsole.log(normal)\nconsole.log(todoStatus(normal))\n\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/index.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
