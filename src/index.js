645d24e82dc67cb8a0478bb941471d38eab1ebf3
garvsl <garvsehgal7@gmail.com>
2023-01-01 02:42:33 -0500

    basic javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('hello')
+import { compareAsc, format } from 'date-fns'
+
+function createProject(name, todo) {
+    return {
+        name: 'Default',
+        todo,
+    }
+}
+
+function createTodo(title, description, dueDate, priority) {
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        status: false,
+    }
+}
+
+function todoStatus(todo) {
+    todo.todo.status = true
+    return todo.todo
+}
+
+const gym = createTodo('gym', 'go to the gym', '1/1/23', 'now')
+const normal = createProject('Default', gym)
+
+console.log(normal)
+console.log(todoStatus(normal))
